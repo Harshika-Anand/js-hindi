5673e71a4472b245e915d7ebaabde7b8876eb96a
Harshika Anand <142821111+Harshika-Anand@users.noreply.github.com>
2024-07-05 20:29:35 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,17 @@
+let score="33abc"
+let valueInNumber=Number(score)
+console.log( valueInNumber);
+//null to number:0
+//underfined and 3abc to number: NaN
+//true:1
+//false:0
+//(Boolean): 1:true,0:false,"":false,"hi":true
+// ***operations*** //
+let value=3
+let  negValue=-value;
+console.log(negValue); 
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");jj
