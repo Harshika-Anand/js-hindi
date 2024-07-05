bf928efad1c036f3086f73b5e5310dc1a9616ba6
Harshika Anand <142821111+Harshika-Anand@users.noreply.github.com>
2024-07-05 09:06:03 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId=14453
+let accountEmail="ha@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+//accountId=2//not allowed
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
