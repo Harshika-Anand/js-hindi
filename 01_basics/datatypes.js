507c13622b2f2328c51069be4f1305c7b681061f
Harshika Anand <142821111+Harshika-Anand@users.noreply.github.com>
2024-07-05 20:57:45 +0000

    datatype

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,23 @@
+//primitive and reference data types
+//primitive: call by value String,Number,null,Boolean,undefined,Symbol,BigInt
+//reference: call by reference array,objects,functions
+const id=Symbol('123')
+const id2=Symbol('123')
+console.log(id);
+console.log(id2);
+console.log(id==id2);
+console.log(id===id2);
+
+//for BigInt, add n to the end of the number
+
+const heroes=["batman","superman"];
+
+let myObj={
+    name:"harshika",
+    age:19,
+}
+
+const myFunction=function(){
+    console.log("Hello World");
+}
+
